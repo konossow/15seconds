ab89b5417480403b687316e00a4d9336e372de59
Plastic <tplasticsamol@gmail.com>
2019-04-13 15:40:49 +0200

    Merge branch 'master' of C:\Users\mlapi\Desktop\15seconds with conflicts.

--- app/src/main/java/com/behealthy/hackathon/fifteenseconds/SettingsActivity.kt
+++ app/src/main/java/com/behealthy/hackathon/fifteenseconds/SettingsActivity.kt
@@ -8,5 +8,6 @@ class SettingsActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
     }
 }

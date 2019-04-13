aa6da8845cac432109ab2195a95639b926dba1d2
Plastic <tplasticsamol@gmail.com>
2019-04-13 13:24:18 +0200

    initial commitm

---
+++ app/src/main/java/com/behealthy/hackathon/fifteenseconds/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.behealthy.hackathon.fifteenseconds
+
+import kotlinx.android.synthetic.main.activity_main.*
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //TextView tv = findViewById<TextView>(R.id.textView)
+    }
+}

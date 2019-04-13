479c6143627b4f4ed9e4019574bac3a6cee9ceb4
Plastic <tplasticsamol@gmail.com>
2019-04-13 13:52:56 +0200

    textView commit

--- app/src/main/java/com/behealthy/hackathon/fifteenseconds/MainActivity.kt
+++ app/src/main/java/com/behealthy/hackathon/fifteenseconds/MainActivity.kt
@@ -11,5 +11,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //TextView tv = findViewById<TextView>(R.id.textView)
+        textView.text = "Hello in 15 seconds!" 
     }
 }

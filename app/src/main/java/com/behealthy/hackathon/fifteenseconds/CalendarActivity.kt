487e5c71c84057df2d04de0f73194fb18cb85ce2
Plastic <tplasticsamol@gmail.com>
2019-04-13 20:08:06 +0200

    calendar part

--- app/src/main/java/com/behealthy/hackathon/fifteenseconds/CalendarActivity.kt
+++ app/src/main/java/com/behealthy/hackathon/fifteenseconds/CalendarActivity.kt
@@ -2,11 +2,19 @@ package com.behealthy.hackathon.fifteenseconds
 
 import android.app.Activity
 import android.os.Bundle
+import com.squareup.timessquare.CalendarPickerView
+import java.util.*
 
 class CalendarActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendar)
+        val calendar_view = findViewById<CalendarPickerView>(R.id.calendar_view)
+        calendar_view.init(Date(119,0,1), Date(119,4,30))
+            .inMode(CalendarPickerView.SelectionMode.MULTIPLE)
+        SettingsSingleton.getSuccededDates().forEach {
+            calendar_view.selectDate(it)
+        }
     }
 }
--- app/src/main/java/com/behealthy/hackathon/fifteenseconds/SettingsSingleton.kt
+++ app/src/main/java/com/behealthy/hackathon/fifteenseconds/SettingsSingleton.kt
@@ -1,10 +1,13 @@
 package com.behealthy.hackathon.fifteenseconds
 
+import java.util.*
 import java.util.regex.Pattern
+import kotlin.collections.ArrayList
 
 object SettingsSingleton {
     var dayAvailableFrom: Int = 17
     var dayAvailableTo: Int = 20
+    private var succededDates: ArrayList<Date> = arrayOf(Date(119,3,10), Date(119,3,11),Date()).toCollection(ArrayList())
     private var exerciseDuration: Int = 15
 
     fun setExerciseDuration(ex_dur: String){
@@ -16,5 +19,16 @@ object SettingsSingleton {
 
     fun getExerciseDuration(): String{
         return this.exerciseDuration.toString() + " seconds"
+    }
+
+    fun setSuccededDates(dates: ArrayList<Date>){
+        this.succededDates.clear()
+        dates.forEach {
+            this.succededDates.add(it)
+        }
+    }
+
+    fun getSuccededDates(): ArrayList<Date>{
+        return this.succededDates
     }
 }

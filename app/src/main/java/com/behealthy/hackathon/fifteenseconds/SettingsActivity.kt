c776d2de2edcff070449f0267b7fa74f725c1f9f
Plastic <tplasticsamol@gmail.com>
2019-04-13 15:55:52 +0200

    main layout fix

--- app/src/main/java/com/behealthy/hackathon/fifteenseconds/SettingsActivity.kt
+++ app/src/main/java/com/behealthy/hackathon/fifteenseconds/SettingsActivity.kt
@@ -9,5 +9,25 @@ class SettingsActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
+        var list_of_items = arrayOf("15 seconds", "30 seconds", "45 second", "60 seconds")
+
+        /*override fun onCreate(savedInstanceState: Bundle?) {
+            spinner!!.setOnItemSelectedListener(this)
+
+            // Create an ArrayAdapter using a simple spinner layout and languages array
+            val aa = ArrayAdapter(this, android.R.layout.simple_spinner_item, list_of_items)
+            // Set layout to use when the list of choices appear
+            aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Set Adapter to Spinner
+            spinner!!.setAdapter(aa)
+        }
+
+        override fun onItemSelected(arg0: AdapterView<*>, arg1: View, position: Int, id: Long) {
+            // use position to know the selected item
+        }
+
+        override fun onNothingSelected(arg0: AdapterView<*>) {
+
+        }*/
     }
 }

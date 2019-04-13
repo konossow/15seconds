0dd59417a5534fc1470497d618ee2aeab59d0cf0
Plastic <tplasticsamol@gmail.com>
2019-04-13 21:52:53 +0200

    beta merge

--- app/src/main/java/com/behealthy/hackathon/fifteenseconds/SettingsSingleton.kt
+++ app/src/main/java/com/behealthy/hackathon/fifteenseconds/SettingsSingleton.kt
@@ -5,10 +5,40 @@ import java.util.regex.Pattern
 import kotlin.collections.ArrayList
 
 object SettingsSingleton {
-    var dayAvailableFrom: Int = 17
-    var dayAvailableTo: Int = 20
+    private var dayAvailableFrom: Int? = null
+    private var dayAvailableTo: Int? = null
     private var succededDates: ArrayList<Date> = arrayOf(Date(119,3,10), Date(119,3,11),Date()).toCollection(ArrayList())
     private var exerciseDuration: Int = 15
+
+    fun getAvailableFrom():String{
+        if(dayAvailableFrom == null){
+            return ""
+        }
+        return this.dayAvailableFrom!!.toString()
+    }
+
+    fun getAvailableTo():String{
+        if(dayAvailableTo == null){
+            return ""
+        }
+        return this.dayAvailableTo!!.toString()
+    }
+
+    fun setAvailableFrom(from:String?){
+        if(from == ""){
+            this.dayAvailableFrom = null
+            return
+        }
+        this.dayAvailableFrom = from!!.toInt()
+    }
+
+    fun setAvailableTo(from:String?){
+        if(from == ""){
+            this.dayAvailableTo = null
+            return
+        }
+        this.dayAvailableTo = from!!.toInt()
+    }
 
     fun setExerciseDuration(ex_dur: String){
         val pattern = Pattern.compile("\\d+")
